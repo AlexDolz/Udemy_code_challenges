8f9bdb4434f8075da5425ed4eb52c06a2455884e
AlexDolz <aleksejdolznikov3@gmail.com>
2023-07-11 16:31:34 +0200

    solution of challenge added

--- challenges.js
+++ challenges.js
@@ -1014,7 +1014,7 @@ Apply this to the team's average scores 😉
 //   });
 // })();
 
-// ******************* Arrays  coding challenge nr 1 ****************
+// ******************* Arrays methods  coding challenge nr 1 ********
 // Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners
 // about their dog's age, and stored the data into an array (one array for each). For
 // now, they are just interested in knowing whether a dog is an adult or a puppy.
@@ -1055,3 +1055,39 @@ const checkDogs = (dogsJulia, dogsKate) => {
 
 // checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+// ****************** Arrays methods coding challenge nr 2 ************
+
+// Let's go back to Julia and Kate's study about dogs. This time, they want to convert
+// dog ages to human ages and calculate the average age of the dogs in their study.
+// Your tasks:
+// Create a function 'calcAverageHumanAge', which accepts an arrays of dog's
+// ages ('ages'), and does the following things in order:
+// 1. Calculate the dog age in human years using the following formula: if the dog is
+// <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old,
+// humanAge = 16 + dogAge * 4
+// 2. Exclude all dogs that are less than 18 human years old (which is the same as
+// keeping dogs that are at least 18 years old)
+// 3. Calculate the average human age of all adult dogs (you should already know
+// from other challenges how we calculate averages 😉)
+// 4. Run the function for both test datasets
+// Test data:
+// § Data 1: [5, 2, 4, 1, 15, 8, 3]
+// § Data 2: [16, 6, 10, 5, 6, 1, 4]
+
+const calcAverageHumanAge = ages => {
+  const humanYears = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  const adultDogs = humanYears.filter(age => age >= 18);
+  // const averageHumanAge =
+  // adultDogs.reduce((acc, cur, i) => acc + cur, 0) / adultDogs.length;
+  // 2 3. (2+3) / 2 = 2.5 === 2/2 + 3/2 = 2.5
+  // 2 solution
+  const averageHumanAge = adultDogs.reduce(
+    (acc, age, i, arr) => acc + age / arr.length,
+    0
+  );
+  return averageHumanAge;
+};
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
